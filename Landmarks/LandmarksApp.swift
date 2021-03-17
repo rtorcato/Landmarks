67cce4c78f9b8ed9af55ae4a405d0334af756f97
Richard Torcato <rtorcato@me.com>
2021-03-17 09:00:42 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Richard Torcato on 2021-03-17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
